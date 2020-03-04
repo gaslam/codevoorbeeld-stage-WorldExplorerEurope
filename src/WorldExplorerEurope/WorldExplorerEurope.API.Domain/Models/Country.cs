04cdc26e2b9a1807578e16246a41fbdf7c662555
Zarpor <ttt.g.com>
2020-03-04 17:25:40 +0100

    Save current progress #6
    I realised that needed to do other stuff before completing this

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldExplorerEurope.API.Domain
+{
+    public class Country
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public long Population { get; set; }
+        public bool IsEUMember { get; set; }
+    }
+}
