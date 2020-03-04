1d56e4a9936c18712d82d26d0c0c29e0ac47c6a7
Zarpor <ttt.g.com>
2020-03-04 18:44:39 +0100

    Add Wishlist Class to Models Fixes #9

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/Models/Wishlist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldExplorerEurope.API.Domain.Models
+{
+    public class Wishlist
+    {
+        public Guid CountryId { get; set; }
+    }
+}
