62388a8b8246dd643babb4ad63d0c2524fa3e92f
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-05 17:54:57 +0100

    Edit Country Model Fixes #6

--- src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/Models/Country.cs
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/Models/Country.cs
@@ -4,9 +4,12 @@ namespace WorldExplorerEurope.API.Domain
 {
     public class Country
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
+        public string FlagUrl { get; set; }
         public long Population { get; set; }
+        public string Capital { get; set; }
+        public string Currency { get; set; }
         public bool IsEUMember { get; set; }
+        public string Description { get; set; }
     }
 }
