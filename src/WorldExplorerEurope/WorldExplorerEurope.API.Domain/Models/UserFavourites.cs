73cbf3f38302c2b6f2d4c309114af78ec846de9a
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-05 18:24:08 +0100

    Change Memory to Favourites Fixes #35

--- src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/Models/UserFavourites.cs
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/Models/UserFavourites.cs
@@ -9,6 +9,6 @@ namespace WorldExplorerEurope.API.Domain.Models
         public Guid UserId { get; set; }
         public Guid MemoryId { get; set; }
         public User User { get; set; }
-        public Memory Memory { get; set; }
+        public Favourites Favourites { get; set; }
     }
 }
