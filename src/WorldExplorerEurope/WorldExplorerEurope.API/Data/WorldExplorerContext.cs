d58efed79db794f457723c78235b44b28ce061f6
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-06 19:20:19 +0100

    Add Context class & change api netcoreapp version #17

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API/Data/WorldExplorerContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldExplorerEurope.API.Domain;
+
+namespace WorldExplorerEurope.API.Data
+{
+    public class WorldExplorerContext : DbContext
+    {
+        public WorldExplorerContext(DbContextOptions<WorldExplorerContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> MyProperty { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
