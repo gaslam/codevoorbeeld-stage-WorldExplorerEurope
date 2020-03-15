9b73fdbfe45290f07017688c0e0f2dcda16455aa
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-15 22:31:52 +0100

    Add Repository & IRepository Classes Fixes #75

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API/Repositories/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldExplorerEurope.API.Domain.Interfaces;
+using WorldExplorerEurope.API.Domain.Models;
+
+namespace WorldExplorerEurope.API.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+    }
+}
