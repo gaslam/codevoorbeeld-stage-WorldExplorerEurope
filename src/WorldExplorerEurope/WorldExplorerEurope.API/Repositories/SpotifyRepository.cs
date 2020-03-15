4cf95fd7fcfa082dc70220ecc5eef22b551ee2af
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-15 22:59:07 +0100

    Add SpotifyRepo & base Context in repo Fixes #76

--- src/WorldExplorerEurope/WorldExplorerEurope.API/Repositories/SpotifyRepository.cs
+++ src/WorldExplorerEurope/WorldExplorerEurope.API/Repositories/SpotifyRepository.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WorldExplorerEurope.API.Data;
-using WorldExplorerEurope.API.Domain.Interfaces;
 using WorldExplorerEurope.API.Domain.Models;
 
 namespace WorldExplorerEurope.API.Repositories
 {
-    public class Repository<T> : IRepository<T> where T : EntityBase
+    public class SpotifyRepository : Repository<SpotifyPlaylist>
     {
         private readonly WorldExplorerContext _worldExplorerContext;
-        public Repository(WorldExplorerContext worldExplorerContext) : base(worldExplorerContext)
+        public SpotifyRepository(WorldExplorerContext worldExplorerContext) : base(worldExplorerContext)
         {
             _worldExplorerContext = worldExplorerContext;
         }
