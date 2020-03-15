73ef712f4fe592554ad2dd7ae6ce743dd2495426
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-15 17:26:15 +0100

    Add UserWishlistDto to DTO Fixes #52

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/DTO/UserWishlistDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldExplorerEurope.API.Domain.DTO
+{
+    public class UserWishlistDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public ICollection<WishlistDto> Wishlists { get; set; }
+    }
+}
