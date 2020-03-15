729947f1d1d4eab45c1618c7065540167b6e1929
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-15 16:52:50 +0100

    Add MemoryDto

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/DTO/MemoryDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldExplorerEurope.API.Domain.DTO
+{
+    public class MemoryDto
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+    }
+}
