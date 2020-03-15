75c6ef5d10d02117459e2851da0f5011918f6103
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-15 16:33:02 +0100

    Create UserDto Fixes #58

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/DTO/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldExplorerEurope.API.Domain.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
