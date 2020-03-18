4de8fd5040078b21f2bc487509003b309d9835c0
gaslam <44768183+gaslam@users.noreply.github.com>
2020-03-18 18:06:58 +0100

    Implement IRepository into Controllers Fixes #86

--- src/WorldExplorerEurope/WorldExplorerEurope.API/Controllers/CountriesController.cs
+++ src/WorldExplorerEurope/WorldExplorerEurope.API/Controllers/CountriesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WorldExplorerEurope.API.Domain.Interfaces;
+using WorldExplorerEurope.API.Domain.Models;
 
 namespace WorldExplorerEurope.API.Controllers
 {
@@ -10,36 +12,10 @@ namespace WorldExplorerEurope.API.Controllers
     [ApiController]
     public class CountriesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly IRepository<Country> _countryRepo;
+        public CountriesController(IRepository<Country> countryRepo)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            _countryRepo = countryRepo;
         }
     }
 }
