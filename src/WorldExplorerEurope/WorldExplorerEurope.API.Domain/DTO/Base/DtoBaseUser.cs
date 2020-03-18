16796c7086d0c1d1244938882f80bf99e755369d
gaslam <44768183+gaslam@users.noreply.github.com>
2020-03-18 16:20:53 +0100

    Add DtoBaseUser #84

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/DTO/Base/DtoBaseUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldExplorerEurope.API.Domain.DTO.Base
+{
+    public abstract class DtoBaseUser
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
--- src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/DTO/UserWishlistDto.cs
+++ src/WorldExplorerEurope/WorldExplorerEurope.API.Domain/DTO/UserWishlistDto.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WorldExplorerEurope.API.Domain.DTO.Base;
 
 namespace WorldExplorerEurope.API.Domain.DTO
 {
-    public class UserWishlistDto
+    public class UserWishlistDto : DtoBaseUser
     {
-        public Guid Id { get; set; }
-        public Guid UserId { get; set; }
         public ICollection<WishlistDto> Wishlists { get; set; }
     }
 }
