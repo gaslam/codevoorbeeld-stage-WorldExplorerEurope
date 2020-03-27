06260a131a0c658bbf75b81a109e7d80cb1a03da
gaslam <gaspard.lammertyn@hotmail.com>
2020-03-27 12:27:13 +0100

    Add WorldExplorerAPIService Fixes #115

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.App/WorldExplorerEurope.App/Services/WorldExplorerAPIService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldExplorerEurope.App.Services
+{
+    public class WorldExplorerAPIService
+    {
+        private readonly string _baseUrl;
+
+        public WorldExplorerAPIService()
+        {
+            _baseUrl = "http:\\192.168.125.225:5000";
+        }
+    }
+}
