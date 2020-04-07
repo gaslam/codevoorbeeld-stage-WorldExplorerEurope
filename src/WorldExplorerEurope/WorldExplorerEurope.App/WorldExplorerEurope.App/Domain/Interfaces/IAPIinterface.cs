56971ed882b6ebaeb89035b452b65b901e3a4f32
gaslam <gaspard.lammertyn@hotmail.com>
2020-04-07 17:18:12 +0200

    Add IAPIinterface

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.App/WorldExplorerEurope.App/Domain/Interfaces/IAPIinterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldExplorerEurope.API.Services.Interface
+{
+    public interface IAPIinterface<Dto>
+    {
+        Task<string> Get(string url);
+        Task<string> Post(string url);
+        Task<string> Put(string url);
+        Task<string> Delete(string url);
+    }
+}
