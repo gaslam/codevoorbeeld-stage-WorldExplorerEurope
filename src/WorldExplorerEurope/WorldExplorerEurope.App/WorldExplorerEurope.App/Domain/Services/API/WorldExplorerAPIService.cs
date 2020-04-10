8d96b40ca799e909e28dffbebd82cde06abadf4c
gaslam <gaspard.lammertyn@hotmail.com>
2020-04-10 19:41:44 +0200

    Remove WorldExplorerAPIService from gitignore

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.App/WorldExplorerEurope.App/Domain/Services/API/WorldExplorerAPIService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldExplorerEurope.App.Domain.Services.API
+{
+    public class WorldExplorerAPIService
+    {
+        public const string BaseUrl = "http://(yourlocalipv4address):5000/api/countries";
+    }
+}
