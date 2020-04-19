84066c9c6361cc3b1b332351cd1e170b409d7555
gaslam <gaspard.lammertyn@hotmail.com>
2020-04-19 22:38:23 +0200

    Add Class & update current packages #164

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.App/WorldExplorerEurope.App.Android/Services/CameraServiceDroid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Plugin.Media.Abstractions;
+using WorldExplorerEurope.App.Domain.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(WorldExplorerEurope.App.Droid.Services.CameraServiceDroid))]
+namespace WorldExplorerEurope.App.Droid.Services
+{
+    public class CameraServiceDroid : ICameraService
+    {
+        public async Task<bool> CheckAndroidCameraPermissions()
+        {
+            return false;
+        }
+
+        public async Task<MediaFile> TakePicture()
+        {
+            return null;
+        }
+
+        public async Task<MediaFile> PickPicture()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/WorldExplorerEurope/WorldExplorerEurope.App/WorldExplorerEurope.App/Domain/Services/CameraService.cs
@@ -0,0 +1,15 @@
+﻿using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldExplorerEurope.App.Domain.Services
+{
+    public interface ICameraService
+    {
+        Task<bool> CheckAndroidCameraPermissions();
+        Task<MediaFile> TakePicture();
+        Task<MediaFile> PickPicture();
+    }
+}
