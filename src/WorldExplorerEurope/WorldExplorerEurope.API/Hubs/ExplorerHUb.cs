ca573e2ca1f2490257b9c403353f6be1ddcceb37
gaslam <gaspard.lammertyn@hotmail.com>
2020-05-10 17:30:25 +0200

    Install SignalR #198

---
+++ src/WorldExplorerEurope/WorldExplorerEurope.API/Hubs/ExplorerHUb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldExplorerEurope.API.Hubs
+{
+    public class ExplorerHub : Hub
+    {
+        public async Task AddFavourite()
+        {
+            await Clients.All.SendAsync("addFavourite");
+        }
+    }
+}
