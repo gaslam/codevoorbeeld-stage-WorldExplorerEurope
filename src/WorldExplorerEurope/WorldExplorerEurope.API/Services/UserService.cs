381a6f2966be28fe0af95949b0806153be4e015c
gaslam <gaspard.lammertyn@hotmail.com>
2020-05-26 20:58:06 +0200

    Expand jwt security by checking Admin role

--- src/WorldExplorerEurope/WorldExplorerEurope.API/Services/UserService.cs
+++ src/WorldExplorerEurope/WorldExplorerEurope.API/Services/UserService.cs
@@ -31,6 +31,7 @@ namespace WorldExplorerEurope.API.Services
                 {
                     new Claim(ClaimTypes.Name, user.Id.ToString()),
                     new Claim(ClaimTypes.Role, user.Role),
+                    new Claim("spotifydj", user.IsSpotifyDj.ToString().ToLower())
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
